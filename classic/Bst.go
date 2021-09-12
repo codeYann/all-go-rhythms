2bd1dbcc34b147a28930012740e7349c6c03dc37
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-09-12 16:32:37 -0300

    Adicionando o arquivo bst.go e seus testes. Operações implementadas: insert, find

---
+++ classic/Bst.go
@@ -0,0 +1,81 @@
+package classic
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Node_Tree struct {
+	Key   int
+	left  *Node_Tree
+	right *Node_Tree
+}
+
+type Tree struct {
+	Root   *Node_Tree
+	length int
+}
+
+func CreateTree() *Tree {
+	return &Tree{Root: nil, length: 0}
+}
+
+func (tree *Tree) InOrder(node *Node_Tree) {
+	if node != nil {
+		tree.InOrder(node.left)
+		fmt.Println(node.Key)
+		tree.InOrder(node.right)
+	}
+
+}
+
+func (tree *Tree) findRecTree(node *Node_Tree, key int) *Node_Tree {
+	if node == nil || node.Key == key {
+		return node
+	}
+
+	if node.Key < key {
+		return tree.findRecTree(node.right, key)
+	}
+	return tree.findRecTree(node.left, key)
+
+}
+
+func (tree *Tree) Find(key int) (*Node_Tree, error) {
+	if tree.Root == nil {
+		return nil, errors.New("Tree is empty!")
+	}
+
+	response := tree.findRecTree(tree.Root, key)
+	if response == nil {
+		return nil, errors.New(fmt.Sprint("Impossible to find", key))
+	} else {
+		return response, nil
+	}
+}
+
+func (tree *Tree) insertRecTree(root, node *Node_Tree) {
+	if node.Key < root.Key {
+		if root.left == nil {
+			root.left = node
+		} else {
+			tree.insertRecTree(root.left, node)
+		}
+	} else {
+		if root.right == nil {
+			root.right = node
+		} else {
+			tree.insertRecTree(root.right, node)
+		}
+	}
+}
+
+func (tree *Tree) Insert(key int) {
+	node := &Node_Tree{Key: key, left: nil, right: nil}
+	if tree.Root == nil {
+		tree.Root = node
+	} else {
+		tree.insertRecTree(tree.Root, node)
+	}
+	tree.length += 1
+}
