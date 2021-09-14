b5b8bd76ec8206b7d538f24392a08624bccd5737
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-09-14 15:29:14 -0300

    Implementação do algoritmo busca em profundidade utilizando o colors approach

---
+++ graph/DFS.go
@@ -0,0 +1,53 @@
+package graph
+
+import "fmt"
+
+type GraphInfo struct {
+	colors []string
+	D      []int
+	F      []int
+}
+
+var mark int = 0
+
+func DFS_VISIT(lista_adj map[int][]int, vertice int, markPointer *int, infos *GraphInfo) {
+	fmt.Println(vertice)
+	*markPointer += 1
+
+	infos.D[vertice] = *markPointer
+	infos.colors[vertice] = "C"
+
+	for _, u := range lista_adj[vertice] {
+		if infos.colors[vertice] == "B" {
+			DFS_VISIT(lista_adj, u, markPointer, infos)
+		}
+	}
+	*markPointer += 1
+	infos.F[vertice] = *markPointer
+	infos.colors[vertice] = "P"
+}
+
+func DFS(g *Graph) *GraphInfo {
+	markValue := &mark
+	*markValue = 0
+
+	infos := &GraphInfo{
+		colors: make([]string, 0),
+		D:      make([]int, 0),
+		F:      make([]int, 0),
+	}
+
+	for i := 0; i < g.GetVerticesNumbers(); i++ {
+		infos.colors = append(infos.colors, "B")
+		infos.D = append(infos.D, 0)
+		infos.F = append(infos.F, 0)
+	}
+
+	for v, _ := range g.Vertices {
+		if infos.colors[v] == "B" {
+			DFS_VISIT(g.Vertices, v, &mark, infos)
+		}
+	}
+
+	return infos
+}
