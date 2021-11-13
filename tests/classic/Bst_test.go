5383fe0262a70464f79da4ba485afe445883caa2
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-11-13 14:36:07 -0300

    Adding Height function test

--- tests/classic/Bst_test.go
+++ tests/classic/Bst_test.go
@@ -31,3 +31,23 @@ func TestInsertBst(t *testing.T) {
 		t.Error("Expected:", 3, "Got:", bst.GetLength())
 	}
 }
+
+func TestHeight(t *testing.T) {
+	bst := BST.CreateTree()
+
+	bst.Insert(30)
+	bst.Insert(25)
+	bst.Insert(40)
+	bst.Insert(28)
+	bst.Insert(15)
+	bst.Insert(6)
+	bst.Insert(9)
+	bst.Insert(39)
+	bst.Insert(42)
+
+	height := bst.Height()
+
+	if height != 4 {
+		t.Error("Expected:", 4, "Got:", height)
+	}
+}
